34db94747df80d0e3e21740e3267a1b0533623f1
Adam <admay202@gmail.com>
2024-08-02 18:33:12 -0500

    Initial Commit

---
+++ Wata/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wata
+//
+//  Created by Adam May on 8/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wata/WataApp.swift
@@ -0,0 +1,17 @@
+//
+//  WataApp.swift
+//  Wata
+//
+//  Created by Adam May on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct WataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
