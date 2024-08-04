db9e71a45cd65a50f029f93e1f5c9bb7ff27017c
Adam <admay202@gmail.com>
2024-08-04 10:20:56 -0500

    added a navigation link to got straight to the CameraView

---
+++ Wata/ConfirmView.swift
@@ -0,0 +1,18 @@
+//
+//  ConfirmView.swift
+//  Wata
+//
+//  Created by Adam May on 8/4/24.
+//
+
+import SwiftUI
+
+struct ConfirmView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConfirmView()
+}
--- Wata/Prompt2View.swift
+++ Wata/Prompt2View.swift
@@ -1,72 +1,109 @@
 import SwiftUI
+import CoreHaptics
 
 struct Prompt2View: View {
+    @State private var isPressed = false
+    @State private var isNavigationActive = false
+    
     var body: some View {
-        VStack(spacing: 20) {
-            // Title and subtitle
-            VStack(alignment: .center, spacing: 5) {
-                Text("Track when you finish drinking your bottle")
-                    .font(.system(size: 30)) // Use your desired font size
-                    .fontWeight(.semibold)
-                    .multilineTextAlignment(.center) // Center-align the text
-            }
-            .frame(width: 350, alignment: .center) // Set a fixed width for the VStack
-            .padding(.horizontal)
-            .offset(y: -120)
-
-            ZStack {
-                GeometryReader { geometry in
-                    // Image with corner radius and white border
-                    Image("water1")
-                        .resizable()
-                        .frame(width: 260, height: 370)
-                        .clipShape(RoundedRectangle(cornerRadius: 20))
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 20)
-                                .stroke(Color.white, lineWidth: 4)
-                        )
-                        .shadow(radius: 10)
-                }
-                .frame(width: 170, height: 230) // Set the frame size to match the images
-                .offset(x: -45, y: -50)
-
-                // Faded black circle with 1 and water droplet emoji
-                ZStack {
-                    Circle()
-                        .fill(Color.brown.opacity(10)) // Faded black color
-                        .frame(width: 60, height: 60) // Size of the circle
-
-                    HStack(spacing: 1) {
-                        Text("1")
-                            .font(.system(size: 24))
-                            .foregroundColor(.white)
-                            .fontWeight(.bold)
-                        Text("💧")
-                            .font(.system(size: 28))
-                    }
-                }
-                .offset(x: -90, y: 165) // Position the circle as needed
-            }
-
-            // Sign in with Apple Button
-            Button(action: {
-                // Action for sign in with Apple button
-            }) {
-                HStack {
-                    Text("Continue")
+        NavigationStack {
+            VStack(spacing: 20) {
+                // Title and subtitle
+                VStack(alignment: .center, spacing: 5) {
+                    Text("Track when you finish drinking your bottle")
+                        .font(.system(size: 30))
                         .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                        .font(.system(size: 20))
+                        .multilineTextAlignment(.center)
                 }
-                .padding()
-                .frame(width: 270, height: 60)
-                .background(Color.black)
-                .cornerRadius(30)
+                .frame(width: 350, alignment: .center)
+                .padding(.horizontal)
+                .offset(y: -120)
+
+                ZStack {
+                    GeometryReader { geometry in
+                        // Image with corner radius and white border
+                        Image("water1")
+                            .resizable()
+                            .frame(width: 260, height: 370)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 20)
+                                    .stroke(Color.white, lineWidth: 4)
+                            )
+                            .shadow(radius: 10)
+                    }
+                    .frame(width: 170, height: 230)
+                    .offset(x: -45, y: -50)
+
+                    // Faded black circle with 1 and water droplet emoji
+                    ZStack {
+                        Circle()
+                            .fill(Color.brown.opacity(0.1))
+                            .frame(width: 60, height: 60)
+
+                        HStack(spacing: 1) {
+                            Text("1")
+                                .font(.system(size: 24))
+                                .foregroundColor(.white)
+                                .fontWeight(.bold)
+                            Text("💧")
+                                .font(.system(size: 28))
+                        }
+                    }
+                    .offset(x: -90, y: 165)
+                }
+
+                // Button that triggers navigation
+                Button(action: {
+                    withAnimation {
+                        isPressed.toggle()
+                    }
+                    triggerHapticFeedback()
+                    isNavigationActive = true
+                }) {
+                    HStack {
+                        Text("Take a photo")
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .font(.system(size: 20))
+                    }
+                    .padding()
+                    .frame(width: 270, height: 60)
+                    .background(Color.black)
+                    .cornerRadius(30)
+                    .scaleEffect(isPressed ? 1.1 : 1.0) // Bounce effect
+                    .shadow(radius: 10)
+                    .buttonStyle(PlainButtonStyle())
+                }
+                .padding(.horizontal)
                 .offset(y: 150)
+
+                // NavigationLink for CameraView
+                NavigationLink(
+                    destination: CameraView(),
+                    isActive: $isNavigationActive
+                ) {
+                    EmptyView() // Empty view to hide the NavigationLink
+                }
+                .hidden() // Hide the NavigationLink
             }
-            .padding(.horizontal)
+            .padding(.vertical, 40)
+            .onAppear {
+                prepareHaptics()
+            }
         }
-        .padding(.vertical, 40)
+    }
+    
+    // Function to prepare haptics
+    func prepareHaptics() {
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.prepare()
+    }
+    
+    // Function to trigger haptic feedback
+    func triggerHapticFeedback() {
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.impactOccurred()
     }
 }
 
